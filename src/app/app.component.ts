9ae2b27a5cde3b66ffe40e2c8260d647c3a79263
Hugo Frias Martinez <a3521110393@alumno.uttehuacan.edu.mx>
2023-01-20 23:06:46 -0600

    Apli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Company } from './app.company';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ConsumoApis';
+
+  Company: Company = new Company;
+  capsule: any[]= [];
+
+  constructor(
+    private http:HttpClient
+    ){};
+
+    ngOnInit(){
+      this.http.get('https://api.spacexdata.com/v4/capsules').subscribe(( capsule:any )=>{
+        this.capsule = capsule;
+      })
+
+      this.getCompany();
+    }
+
+    getCompany(){
+      this.http.get('https://api.spacexdata.com/v4/company').subscribe(( data:any )=>{
+        this.Company = data;
+      });
+    }
+
+
 }
